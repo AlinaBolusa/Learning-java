ac8b5548ee15f05c6a5db0a21d784f5f60e8373a
AlinaBolusa <bolusaalina@gmail.com>
2024-06-09 12:57:10 +0300

    [classwork/06062024]: Learn how to work with statements and methods.

--- src/main/java/lv/acodemy/classroom/PlayWithStatements.java
+++ src/main/java/lv/acodemy/classroom/PlayWithStatements.java
@@ -3,77 +3,65 @@ package lv.acodemy.classroom;
 public class PlayWithStatements {
     public static void main(String[] args) {
 
-        // if / ( if-else) / if - else -if
+        // If / (if-else) / if-else-if statements
 
         /*
-
-        if(condition) {
-
-        // code to be executed
+        if (condition) {
+            // code to be executed
         }
-
          */
 
-        // Exercise: if a >5 => System.out.println("This is true!");
-
+        // Exercise 1: if a > 5 => System.out.println("This is true!");
         int a = 10;
         if (a > 5) {
             System.out.println("This is true!");
         }
 
-        // exercise 2: write program that will check if veriable y is even
-
+        // Exercise 2: Write a program that will check if variable y is even
         int y = 12;
         if (y % 2 == 0) {
-            System.out.println("y is even number");
+            System.out.println("y is an even number");
         }
 
-        // exercise 3: write program that will check if number is equal to 50;
-
+        // Exercise 3: Write a program that will check if the number is equal to 50
         int b = 50;
         if (b == 50) {
             System.out.println("b is equal to 50");
         }
 
-        // >,<. <==,==>,==,!= ( comparison operators);
-
+        // Comparison operators: >, <, <=, >=, ==, !=
         if (b != y) {
             System.out.println("b is not equal to y");
         }
-        //Boolean -> is or has (prefix)
 
+        // Boolean -> is or has (prefix)
         boolean isSummer = true;
         if (isSummer) {
-            System.out.println("It`s time to go on vacation!");
-
-
+            System.out.println("It's time to go on vacation!");
         }
 
         // IF/ELSE
 
         /*
-        if(condition) {
-        will execute code if statement is true;
-        }else{
-        will execute code if statement is false;
-        }
-
-         */
-        // Exercise 4: write program that will check if number is positive;
-
-
-        int z = 10;
-
-        if (z >= 0) {
-            System.out.println("Z is positive number");
+        if (condition) {
+            // will execute code if statement is true
         } else {
-            System.out.println("Z is negative number");
+            // will execute code if statement is false
+        }
+         */
+
+        // Exercise 4: Write a program that will check if a number is positive
+        int z = 10;
+        if (z >= 0) {
+            System.out.println("Z is a positive number");
+        } else {
+            System.out.println("Z is a negative number");
         }
 
-        // Exercise 5: if name equal John, then print out - We found John! or variabls contains John
+        // Exercise 5: If name equals John, then print out - "We found John!" or variable contains name
         String name = "Mary";
         if (name.equals("John")) {
-            System.out.println("Variable contains name");
+            System.out.println("We found John!");
         } else {
             System.out.println("Variable contains name: " + name);
         }
@@ -81,93 +69,68 @@ public class PlayWithStatements {
         // IF - ELSE - IF - ELSE
 
         /*
-        if(condition1) {
-        if condition1 is true => this code block will be executed }
-        else if (condition2){
-        if condition2 is true => this code block will be executed }
-        else{
-        if no condition is not => this code block will be executed
+        if (condition1) {
+            // if condition1 is true => this code block will be executed
+        } else if (condition2) {
+            // if condition2 is true => this code block will be executed
+        } else {
+            // if no condition is true => this code block will be executed
         }
          */
 
-        // Exercise 6: write program that will check if variable >10, check variable == 10, and all other is less then 10;
-
+        // Exercise 6: Write a program that will check if variable > 10, check variable == 10, and all other cases as less than 10
         int e = 12;
         if (e > 10) {
             System.out.println("Variable is more than 10");
         } else if (e == 10) {
-            System.out.println("Variable is equal 10");
+            System.out.println("Variable is equal to 10");
         } else {
             System.out.println("Variable is less than 10");
         }
 
-        // exercise 7: create array with numbers, iterate through it and check if number is positive, negative or equal to 0;
+        // Exercise 7: Create an array with numbers, iterate through it and check if the number is positive, negative, or equal to 0
         int[] numbers = {10, -5, 0, 25, -30, 15, -10, 5, -20};
-        for (int i = 0; i < numbers.length; i++) {
-            int number = numbers[i];
+        for (int number : numbers) {
             if (number > 0) {
-                System.out.printf("Number: %d is positive number\n", number);
+                System.out.printf("Number: %d is a positive number\n", number);
             } else if (number < 0) {
-                System.out.printf("Number: %d is negative number\n", number);
+                System.out.printf("Number: %d is a negative number\n", number);
             } else {
                 System.out.printf("Number: %d is equal to 0\n", number);
             }
         }
 
-
-        // Exercise 8: excellent ( 90-100) / good (75-89) / OK (50-74) / NOK (0-49)
-
+        // Exercise 8: Grades classification - excellent (90-100) / good (75-89) / OK (50-74) / NOK (0-49)
         int[] grades = {95, 82, 74, 56, 48, 91, 87, 66, 77, 45};
         for (int grade : grades) {
             if (grade >= 90 && grade <= 100) {
-                System.out.printf("Grade %d is Excellent \n", grade);
-            } else if (grade >= 75 && grade <= 90) {
-
-                System.out.printf("Grade %d is Good \n", grade);
-            } else if (grade >= 50 && grade <= 75) {
-
-                System.out.printf("Grade %d is OK \n", grade);
-            } else if (grade >= 0 && grade <= 50) {
-                System.out.printf("Grade %d is NOK \n", grade);
+                System.out.printf("Grade %d is Excellent\n", grade);
+            } else if (grade >= 75 && grade <= 89) {
+                System.out.printf("Grade %d is Good\n", grade);
+            } else if (grade >= 50 && grade <= 74) {
+                System.out.printf("Grade %d is OK\n", grade);
+            } else if (grade >= 0 && grade <= 49) {
+                System.out.printf("Grade %d is NOK\n", grade);
             }
         }
 
-        // exercise 9: create temperature classifier
-        // (Temperature < -10 (Holodno) / from 0 to 10 (prohladno) /
-        // from 10 to 20 (teplo) /
-        // from 20 to 30 (zharko) / >30 - ocen zarko
+        // Exercise 9: Temperature classifier
+        // (Temperature < -10 (Holodno) / from 0 to 10 (Prohladno) /
+        // from 10 to 20 (Teplo) /
+        // from 20 to 30 (Zharko) / > 30 - Ochen zharko)
 
-        int[] temperatures = {-5, 12, 18, 8, 21, 29, 30, 1, 45, 0};
+        int[] temperatures = {-5, 12, 18, 8, 21, 29, 30, 1, 45, 9};
         for (int temperature : temperatures) {
             if (temperature > 30) {
-                System.out.printf("Temperature %d is ochen zarko\n", temperature);
+                System.out.printf("Temperature %d is Ochen zharko\n", temperature);
             } else if (temperature > 20) {
-                System.out.printf("Temperature %d is zarko\n", temperature);
+                System.out.printf("Temperature %d is Zharko\n", temperature);
             } else if (temperature > 10) {
-                System.out.printf("Temperature %d is teplo\n", temperature);
+                System.out.printf("Temperature %d is Teplo\n", temperature);
             } else if (temperature > 0) {
-                System.out.printf("Temperature %d is prohladno\n", temperature);
+                System.out.printf("Temperature %d is Prohladno\n", temperature);
             } else {
-                System.out.printf("Temperature %d is holodno\n", temperature);
-            }
-        }
-
-        int[] temperatures1 = {-5, 12, 18, 8, 21, 29, 30, 1, 45, 9};
-        for (int temperature : temperatures1) {
-            if (temperature >= 30 && temperature <= 45) {
-
-                System.out.printf("Temperature %d is Ocen zarko \n", temperature);
-
-            } else if (temperature >= 20 && temperature <= 30) {
-
-                System.out.printf("Temperature %d is zarko \n", temperature);
-            } else if (temperature >= 10 && temperature <= 20) {
-
-                System.out.printf("Temperature %d is teplo \n", temperature);
-            } else if (temperature >= 0 && temperature <= 10) {
-                System.out.printf("Temperature %d is prohladno \n", temperature);
-            } else if (temperature >= -10 && temperature <= 0) {
-                System.out.printf("Temperature %d is holodno \n", temperature);
+                System.out.printf("Temperature %d is Holodno\n", temperature);
             }
         }
     }
